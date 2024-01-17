6da315fd0b8671e4e0e5b8d7843a028578b81a50
axelNTI <axel.thornberg@elev.ga.ntig.se>
2024-01-17 08:46:50 +0100

    2024-01-17

--- A2/BMI.java
+++ A2/BMI.java
@@ -7,12 +7,16 @@ public class BMI {
     public static void main(String[] args) {
         Scanner inputWeightScanner = new Scanner(System.in);
         System.out.println("Enter your weight in kilograms");
-        double weight = inputWeightScanner.nextLine();
+        double weight = Double.parseDouble(inputWeightScanner.nextLine());
+
         Scanner inputHeightScanner = new Scanner(System.in);
         System.out.println("Enter your height in meters");
-        double height = inputHeightScanner.nextLine();
+        double height = Double.parseDouble(inputHeightScanner.nextLine());
+        inputWeightScanner.close();
+        
+        inputHeightScanner.close();
         double BMI = weight / Math.pow(height, 2);
-        System.out.println("Your BMI is" + BMI);
+        System.out.println("Your BMI is " + BMI);
     }
 
 }
